ba97e821f5dbdac5397beb2480c801a6e1b53b93
jonallen0 <jayayedev@gmail.com>
2016-11-10 10:39:56 +0000

    Initial commit from the atomist bot

---
+++ src/main/java/com/jcpa/RequestReceiverApplication.java
@@ -0,0 +1,12 @@
+package com.jcpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestReceiverApplication.class, args);
+    }
+}
